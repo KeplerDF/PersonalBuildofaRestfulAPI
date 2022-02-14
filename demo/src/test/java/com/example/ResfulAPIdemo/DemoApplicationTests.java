d20367170bc455adb5e3b2a734a0224c9122e12a
KeplerDF <91970751+KeplerDF@users.noreply.github.com>
2022-02-14 16:05:13 +0000

    Add files via upload

--- demo/src/test/java/com/example/ResfulAPIdemo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/ResfulAPIdemo/DemoApplicationTests.java
@@ -3,11 +3,42 @@ package com.example.ResfulAPIdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void sensorTest() {
+		WeatherInfoController instanceofcontrollerrun = new WeatherInfoController();
+
+		Sensor actual = instanceofcontrollerrun.sensor("Ireland", "Dublin");
+
+		Sensor expected = new Sensor(1, "Ireland", "Dublin");
+
+		assertEquals(actual.toString(), expected.toString());
+	}
+
+	@Test
+	public void updateTest() {
+		WeatherInfoController instanceofcontrollerrun = new WeatherInfoController();
+
+		instanceofcontrollerrun.updatesensor("1", "30", "30");
+		ArrayList<Sensor> listinstance = instanceofcontrollerrun.sensorListGetter();
+		Sensor actual = null;
+		try{
+			actual = listinstance.get(0);
+		}catch(Exception e){
+
+		}
+
+		Sensor expected = new Sensor(1, "", "") ;
+		expected.addPoint("30", "30");
+
+		assertNotEquals(actual, expected);
 	}
 
 }
