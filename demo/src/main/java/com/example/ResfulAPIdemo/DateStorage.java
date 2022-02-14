19095c8ddcb9cadbeec9c3bc0ea0c2d37b4f26d1
KeplerDF <91970751+KeplerDF@users.noreply.github.com>
2022-02-14 03:54:47 +0000

    Update DateStorage.java

--- demo/src/main/java/com/example/ResfulAPIdemo/DateStorage.java
+++ demo/src/main/java/com/example/ResfulAPIdemo/DateStorage.java
@@ -2,18 +2,25 @@ package com.example.ResfulAPIdemo;
 
 import java.util.ArrayList;
 
+/*
+    This object exists to store the metrics in a much more managable object. 
+    This is done because one sensor can have many instances of metrics so this was built to contain every instance but in a more managable format
+*/
+
 public class DateStorage {
 
     private ArrayList<Metrics> metriclist = new ArrayList<>();
 
     public DateStorage(){
     }
-
+    
+//  This adds a new metric to the Arraylist
     public void addMetric(String temperature, String humidity){
         Metrics met = new Metrics(temperature,humidity);
         metriclist.add(met);
     }
-
+    
+//  Returns the entire arraylist
     public ArrayList<Metrics> getMetriclist(){
 
         return metriclist;
