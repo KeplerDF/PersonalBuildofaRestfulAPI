30ad73a5ad2586f69d4e011fdd1721a19594ea37
KeplerDF <91970751+KeplerDF@users.noreply.github.com>
2022-02-14 03:56:38 +0000

    Update DemoApplication.java

--- demo/src/main/java/com/example/ResfulAPIdemo/DemoApplication.java
+++ demo/src/main/java/com/example/ResfulAPIdemo/DemoApplication.java
@@ -6,7 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class DemoApplication{
-
+	
+	
+//	This acts as the executable and the Springbootapplication annotation finds all the controllers as well as setting the default configuration.
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
