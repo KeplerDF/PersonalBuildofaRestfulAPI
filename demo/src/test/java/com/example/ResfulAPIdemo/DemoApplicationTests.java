951cc76335ff22eb11fc9bbbe1e1c86ed069a6f3
KeplerDF <91970751+KeplerDF@users.noreply.github.com>
2022-02-14 16:10:41 +0000

    Update DemoApplicationTests.java

--- demo/src/test/java/com/example/ResfulAPIdemo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/ResfulAPIdemo/DemoApplicationTests.java
@@ -8,9 +8,12 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
+
+//	This serves as the basic JUnit coverage for some of my methods
 @SpringBootTest
 public class DemoApplicationTests {
 
+	//This method tests weather the sensor method actually creates the sensor object properly when it is run.
 	@Test
 	public void sensorTest() {
 		WeatherInfoController instanceofcontrollerrun = new WeatherInfoController();
@@ -22,6 +25,11 @@ public class DemoApplicationTests {
 		assertEquals(actual.toString(), expected.toString());
 	}
 
+	
+	//	This method serves to test weather the update for a sensor activetes however running the code does not update the 
+	//	right isntance of the arraylist which means that it causes an out of bounds exception when I try to search for that instance of this
+	// 	This does not show up in the http request.
+	// 	This is also an issue I encountered when building my infoGetter method
 	@Test
 	public void updateTest() {
 		WeatherInfoController instanceofcontrollerrun = new WeatherInfoController();
